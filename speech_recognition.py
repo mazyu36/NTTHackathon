c114497eef93a0e70a63da08d7d06e729388337f
maz <yiyth.fcb6@gmail.com>
2023-06-01 13:31:19 +0900

    音声認識+OpenAIへのリクエストを実装

--- speech_recognition.py
+++ speech_recognition.py
@@ -1,5 +1,12 @@
+import openai
 import os
 import azure.cognitiveservices.speech as speechsdk
+
+# Note: The openai-python library support for Azure OpenAI is in preview.
+openai.api_type = "azure"
+openai.api_base = "https://matsudayui.openai.azure.com/"
+openai.api_version = "2023-03-15-preview"
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 def recognize_from_microphone():
@@ -20,9 +27,11 @@ def recognize_from_microphone():
 
     if speech_recognition_result.reason == speechsdk.ResultReason.RecognizedSpeech:
         print("Recognized: {}".format(speech_recognition_result.text))
+        return speech_recognition_result.text
     elif speech_recognition_result.reason == speechsdk.ResultReason.NoMatch:
         print("No speech could be recognized: {}".format(
             speech_recognition_result.no_match_details))
+        return None
     elif speech_recognition_result.reason == speechsdk.ResultReason.Canceled:
         cancellation_details = speech_recognition_result.cancellation_details
         print(
@@ -33,6 +42,31 @@ def recognize_from_microphone():
                 "Error details: {}".format(
                     cancellation_details.error_details))
             print("Did you set the speech resource key and region values?")
+        return None
 
 
-recognize_from_microphone()
+def get_from_openai(text):
+    response = openai.ChatCompletion.create(engine="gpt-35-turbo",
+                                            messages=[{"role": "system",
+                                                       "content": "You are an AI assistant that helps people find information."},
+                                                      {"role": "user",
+                                                       "content": text},
+                                                      {"role": "assistant",
+                                                       "content": ""}],
+                                            temperature=0.7,
+                                            max_tokens=800,
+                                            top_p=0.95,
+                                            frequency_penalty=0,
+                                            presence_penalty=0,
+                                            stop=None)
+
+    answer_text = response["choices"][0]["message"]["content"]
+    print(answer_text)
+    return answer_text
+
+
+# 音声からテキストを抽出
+text = recognize_from_microphone()
+
+# 音声で抽出したテキストをOpenAIにリクエストして結果を取得
+openai_response = get_from_openai(text)
